0f101a7763de026075dfe98f751a1ab2e5a5a727
Rezvanova Daria <daria.rezv@gmail.com>
2019-02-14 03:34:48 +0600

    Tasks 1-5 are done

--- src/operationExecutor.js
+++ src/operationExecutor.js
@@ -1,160 +1,167 @@
 'use strict';
 
 class OperationExecutor {
-  constructor() {
-    this.state = {
-      0: this.firstTaskExecute.bind(this),
-      1: this.secondTaskExecute.bind(this),
-      2: this.thirdTaskExecute.bind(this),
-      3: this.fourthTaskExecute.bind(this),
-      4: this.fifthTaskExecute.bind(this),
-      5: this.sixthTaskExecute.bind(this),
-      6: this.seventhTaskExecute.bind(this),
-      7: this.eighthTaskExecute.bind(this),
-      8: this.ninthTaskExecute.bind(this),
-      9: this.tenthTaskExecute.bind(this),
-    };
-  }
+    constructor() {
+        this.state = {
+            0: this.firstTaskExecute.bind(this),
+            1: this.secondTaskExecute.bind(this),
+            2: this.thirdTaskExecute.bind(this),
+            3: this.fourthTaskExecute.bind(this),
+            4: this.fifthTaskExecute.bind(this),
+            5: this.sixthTaskExecute.bind(this),
+            6: this.seventhTaskExecute.bind(this),
+            7: this.eighthTaskExecute.bind(this),
+            8: this.ninthTaskExecute.bind(this),
+            9: this.tenthTaskExecute.bind(this),
+        };
+    }
 
-  /**
-   * Execute some transformation of incoming arg
-   * @param actionType – type of transformation
-   * @param arg – incoming arg
-   * @returns object with result
-   */
-  execute(actionType, arg) {
-    return this.state[actionType](arg);
-  }
-
-  /**
-   * First task of homework
-   * @param arg – object with value that you should clone
-   * arg = { obj1: { ... } }
-   * @returns object that contains source object and his modified clone
-   */
-  firstTaskExecute(arg) {
     /**
-     * Place your code here
+     * Execute some transformation of incoming arg
+     * @param actionType – type of transformation
+     * @param arg – incoming arg
+     * @returns object with result
      */
-    return null /* variable with result */;
-  }
+    execute(actionType, arg) {
+        return this.state[actionType](arg);
+    }
 
-  /**
-   * Second task of homework
-   * @param arg – object with values that you should combine
-   * arg = { obj1: { ... }, obj2: { ... } }
-   * @returns object that contains source objects and their combined and modified clone
-   */
-  secondTaskExecute(arg) {
     /**
-     * Place your code here
+     * First task of homework
+     * @param arg – object with value that you should clone
+     * arg = { obj1: { ... } }
+     * @returns object that contains source object and his modified clone
      */
-    return null /* variable with result */;
-  }
+    firstTaskExecute(arg) {
+        //let copy = Object.assign({}, arg);
+        return JSON.parse(JSON.stringify(arg)) /* variable with result */;
+    }
 
-  /**
-   * Third task of homework
-   * @param arg – object with value that you should modify
-   * arg = { obj1: { ... } }
-   * @returns object that contains modified source object
-   */
-  thirdTaskExecute(arg) {
     /**
-     * Place your code here
+     * Second task of homework
+     * @param arg – object with values that you should combine
+     * arg = { obj1: { ... }, obj2: { ... } }
+     * @returns object that contains source objects and their combined and modified clone
      */
-    return null /* variable with result */;
-  }
+    secondTaskExecute(arg) {
+        return {...arg.obj1, ...arg.obj2};
+        /* variable with result */
+        ;
+    }
 
-  /**
-   * Fourth task of homework
-   * @param arg – object with value that contains relatives
-   * arg = { obj1: { ... relatives: [ ... ] ... } }
-   * @returns object that contains array of string with female relatives
-   */
-  fourthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Third task of homework
+     * @param arg – object with value that you should modify
+     * arg = { obj1: { ... } }
+     * @returns object that contains modified source object
      */
-    return null /* variable with result */;
-  }
+    thirdTaskExecute(arg) {
+        let relatives = arg.obj1.relatives;
+        relatives.forEach((item) => {
+            if (item.firstName == "Lena" || item.firstName == "Tanya") {
+                item.gender = "female";
+            }
+            if (item.firstName == "Roma") {
+                item.gender = "male";
+            }
+        });
+        return relatives;
+    }
 
-  /**
-   * Fifth task of homework
-   * @param arg – object which contains new color of the button and the class of it
-   * arg = { color: '...', className: '...' }
-   * @returns string which contains the class of the button and current color
-   */
-  fifthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Fourth task of homework
+     * @param arg – object with value that contains relatives
+     * arg = { obj1: { ... relatives: [ ... ] ... } }
+     * @returns object that contains array of string with female relatives
      */
-    return '';
-  }
+    fourthTaskExecute(arg) {
+        let relatives = arg.obj1.relatives;
+        let result = [];
+        relatives.forEach((item) => {
+            if (item.gender == "female") {
+                const name = item.firstName;
+                result.push(`${name}, hello! We are glad to see\n`);
+            }
+        });
+        return result /* variable with result */;
+    }
 
-  /**
-   * Sixth task of homework
-   * @param arg – object with values that you should handle
-   * arg = { obj1: { ... } }
-   * @returns object that contains array of items that match the hostname on which the application is running
-   */
-  sixthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Fifth task of homework
+     * @param arg – object which contains new color of the button and the class of it
+     * arg = { color: '...', className: '...' }
+     * @returns string which contains the class of the button and current color
      */
-    return null;
-  }
+    fifthTaskExecute(arg) {
+        let element = document.getElementsByClassName(arg.className)[0];
+        element.style.backgroundColor = arg.color;
+        return `className: ${arg.className}; color: ${arg.color}`;
+    }
 
-  /**
-   * Seventh task of homework
-   * @param arg – object which contains simple key-value pairs
-   * arg = { obj1: { key: value } }
-   * @returns obj that contains swap pairs ('value: key')
-   */
-  seventhTaskExecute(arg) {
     /**
-     * Place your code here
+     * Sixth task of homework
+     * @param arg – object with values that you should handle
+     * arg = { obj1: { ... } }
+     * @returns object that contains array of items that match the hostname on which the application is running
      */
-    return null;
-  }
+    sixthTaskExecute(arg) {
+        /**
+         * Place your code here
+         */
+        return null;
+    }
 
-  /**
-   * Eighth task of homework
-   * @param arg – object which contains two array
-   * arg = { obj1: { ... } }
-   * @returns obj that built using array's values
-   */
-  eighthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Seventh task of homework
+     * @param arg – object which contains simple key-value pairs
+     * arg = { obj1: { key: value } }
+     * @returns obj that contains swap pairs ('value: key')
      */
-    return null;
-  }
+    seventhTaskExecute(arg) {
+        /**
+         * Place your code here
+         */
+        return null;
+    }
 
-  /**
-   * Ninth task of homework
-   * @param arg – object which contains array of users
-   * arg = { obj1: { users: [...] } }
-   * @returns obj that contains pairs id: obj with this id
-   */
-  ninthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Eighth task of homework
+     * @param arg – object which contains two array
+     * arg = { obj1: { ... } }
+     * @returns obj that built using array's values
      */
-    return null;
-  }
+    eighthTaskExecute(arg) {
+        /**
+         * Place your code here
+         */
+        return null;
+    }
 
-  /**
-   * Tenth task of homework
-   * @param arg – object which contains class of item and empty array
-   * arg = { obj1: { ... } }
-   * @returns obj that contains the array with info about children of the node
-   */
-  tenthTaskExecute(arg) {
     /**
-     * Place your code here
+     * Ninth task of homework
+     * @param arg – object which contains array of users
+     * arg = { obj1: { users: [...] } }
+     * @returns obj that contains pairs id: obj with this id
      */
-    return null;
-  }
+    ninthTaskExecute(arg) {
+        /**
+         * Place your code here
+         */
+        return null;
+    }
+
+    /**
+     * Tenth task of homework
+     * @param arg – object which contains class of item and empty array
+     * arg = { obj1: { ... } }
+     * @returns obj that contains the array with info about children of the node
+     */
+    tenthTaskExecute(arg) {
+        /**
+         * Place your code here
+         */
+        return null;
+    }
 }
 
 export default OperationExecutor;
